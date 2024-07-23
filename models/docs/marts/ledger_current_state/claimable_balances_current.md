261d6bb25dc9f6c91940daa2cd264ff33d8162d7
Amisha Singla <amishas157@gmail.com>
2024-07-23 13:21:20 -0500

    add docs docs
    lint
    
    lint

--- models/docs/marts/ledger_current_state/claimable_balances_current.md
+++ models/docs/marts/ledger_current_state/claimable_balances_current.md
@@ -5,3 +5,7 @@
 The `claimable_balances_current` table is a nightly snapshotted table that represents the current state of claimable balances ledger entries.
 
 {% enddocs %}
+
+{% docs flags %}
+Denotes the enabling and disabling of certain asset issuer privileges
+{% enddocs %}
--- models/marts/ledger_current_state/claimable_balances_current.sql
+++ models/marts/ledger_current_state/claimable_balances_current.sql
@@ -12,28 +12,28 @@
 with
     current_balance as (
         select
-            cb.balance_id,
-            cb.claimants,
-            cb.asset_type,
-            cb.asset_code,
-            cb.asset_issuer,
-            cb.asset_id,
-            cb.asset_amount,
-            cb.sponsor,
-            cb.flags,
-            cb.last_modified_ledger,
-            cb.ledger_entry_change,
-            cb.deleted,
-            cb.batch_id,
-            cb.batch_run_date,
-            cb.batch_insert_ts,
-            cb.closed_at,
-            cb.ledger_sequence,
+            cb.balance_id
+            , cb.claimants
+            , cb.asset_type
+            , cb.asset_code
+            , cb.asset_issuer
+            , cb.asset_id
+            , cb.asset_amount
+            , cb.sponsor
+            , cb.flags
+            , cb.last_modified_ledger
+            , cb.ledger_entry_change
+            , cb.deleted
+            , cb.batch_id
+            , cb.batch_run_date
+            , cb.batch_insert_ts
+            , cb.closed_at
+            , cb.ledger_sequence
             , row_number()
-            over (
-                partition by cb.balance_id
-                order by cb.closed_at desc
-            ) as rn
+                over (
+                    partition by cb.balance_id
+                    order by cb.closed_at desc
+                ) as rn
         from {{ ref('stg_claimable_balances') }} as cb
         {% if is_incremental() %}
             -- limit the number of partitions fetched incrementally
@@ -46,23 +46,23 @@ with
     )
 
 select
-    balance_id,
-    claimants,
-    asset_type,
-    asset_code,
-    asset_issuer,
-    asset_id,
-    asset_amount,
-    sponsor,
-    flags,
-    last_modified_ledger,
-    ledger_entry_change,
-    deleted,
-    batch_id,
-    batch_run_date,
-    closed_at,
-    ledger_sequence,
-    batch_insert_ts as upstream_insert_ts,
-    current_timestamp() as batch_insert_ts
+    balance_id
+    , claimants
+    , asset_type
+    , asset_code
+    , asset_issuer
+    , asset_id
+    , asset_amount
+    , sponsor
+    , flags
+    , last_modified_ledger
+    , ledger_entry_change
+    , deleted
+    , batch_id
+    , batch_run_date
+    , closed_at
+    , ledger_sequence
+    , batch_insert_ts as upstream_insert_ts
+    , current_timestamp() as batch_insert_ts
 from current_balance
 where rn = 1
