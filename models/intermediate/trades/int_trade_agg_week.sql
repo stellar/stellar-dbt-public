f50c40b87455de3f6f3daf9a0ce4956a14dfcece
sydneynotthecity <sydney@stellar.org>
2024-09-05 08:51:08 -0500

    Lint int_trade_agg_week.sql

--- models/intermediate/trades/int_trade_agg_week.sql
+++ models/intermediate/trades/int_trade_agg_week.sql
@@ -25,8 +25,8 @@ with
             , buying_amount
         from {{ ref('stg_history_trades') }}
         where
-            ledger_closed_at < timestamp_add('{{ dbt_airflow_macros.ts(timezone=none) }}', INTERVAL 1 DAY)
-            and ledger_closed_at >= timestamp_sub('{{ dbt_airflow_macros.ts(timezone=none) }}', INTERVAL 8 DAY)
+            ledger_closed_at < timestamp_add('{{ dbt_airflow_macros.ts(timezone=none) }}', interval 1 day)
+            and ledger_closed_at >= timestamp_sub('{{ dbt_airflow_macros.ts(timezone=none) }}', interval 8 day)
     )
 
     /* duplicates trades in order to obtain all trades between an asset pair, regardless
