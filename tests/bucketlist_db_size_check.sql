acfc893b5709802a703c12aade4bf48fb5512e51
Amisha Singla <amishas157@gmail.com>
2024-09-11 13:26:18 +0530

    remove trailing  whitespace

--- tests/bucketlist_db_size_check.sql
+++ tests/bucketlist_db_size_check.sql
@@ -11,7 +11,7 @@ with bucketlist_db_size as (
   from `crypto-stellar.crypto_stellar.history_ledgers`
   where closed_at >= TIMESTAMP_SUB('{{ dbt_airflow_macros.ts(timezone=none) }}', INTERVAL 1 HOUR )
   -- alert when the bucketlist has grown larger than 12 gb
-    and total_byte_size_of_bucket_list / 1000000000 >= 12 
+    and total_byte_size_of_bucket_list / 1000000000 >= 12
 )
 
 select * from bucketlist_db_size;
