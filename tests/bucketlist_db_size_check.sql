8dc3b54bc8d6409b9a1fa7dd9ac0d77961d2795d
Amisha Singla <amishas157@gmail.com>
2024-09-11 13:14:43 +0530

    Update generic tests

--- tests/bucketlist_db_size_check.sql
+++ tests/bucketlist_db_size_check.sql
@@ -9,19 +9,9 @@ with bucketlist_db_size as (
     closed_at,
     total_byte_size_of_bucket_list / 1000000000 as bl_db_gb
   from `crypto-stellar.crypto_stellar.history_ledgers`
-  where closed_at >= current_timestamp() - interval 1 hour
+  where closed_at >= TIMESTAMP_SUB('{{ dbt_airflow_macros.ts(timezone=none) }}', INTERVAL 1 HOUR )
   -- alert when the bucketlist has grown larger than 12 gb
     and total_byte_size_of_bucket_list / 1000000000 >= 12 
 )
 
-select 
-if (
-  (select count(*)
-    from bucketlist_db_size
-  ) = 0,
-  --Return when True:
-  'Bucketlist is under 12GB',
-  -- Return Alert when False:
-  ERROR('BucketlistDB size has exceeded 12GB!'))
-  ;
-  select true;
+select * from bucketlist_db_size;
--- tests/sorobon_surge_pricing_check.sql
+++ tests/sorobon_surge_pricing_check.sql
@@ -9,19 +9,9 @@ with surge_pricing_check as (
     ledger_sequence,
     closed_at
 from `crypto-stellar.crypto_stellar_dbt.enriched_history_operations_soroban`
-where closed_at >= current_timestamp - interval 1 hour
+where closed_at >= TIMESTAMP_SUB('{{ dbt_airflow_macros.ts(timezone=none) }}', INTERVAL 1 HOUR )
   -- inclusion fees over 100 stroops indicate surge pricing on the network
   and inclusion_fee_charged > 100
 )
 
-select 
-if (
-  (select count(*)
-    from surge_pricing_check
-  ) = 0,
-  --Return when True:
-  'Network is in normal pricing',
-  -- Return Alert when False:
-  ERROR('Network has entered surge pricing for Soroban'))
-  ;
-  select true; 
+select * from surge_pricing_check;
