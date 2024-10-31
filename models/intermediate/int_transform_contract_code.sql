529e5866a082820b09556d7daa578162079200cf
harsha-stellar-data <sriharsha.pasupuleti@stellar.org>
2024-10-31 18:39:28 -0400

    Combined incremental and transformation models into one to prevent dbt_project_evaluator specs

--- models/intermediate/int_transform_contract_code.sql
+++ models/intermediate/int_transform_contract_code.sql
@@ -12,18 +12,19 @@ Model: int_transform_contract_code
 
 Description:
 ------------
-This intermediate model applies transformations to the contract code, calculates new fields,
-and performs deduplication based on the transformed data.
+This intermediate model combines incremental loading and transformations for contract code data.
+It processes new or updated data, calculates new fields, and performs deduplication.
 
-Load Type: Full Truncate and Reload
+Load Type: Truncate and Reload
 
 Features:
 ---------
-1. Calculates contract_create_ts and contract_delete_ts from staging data.
-2. Joins the calculated timestamps with the incremental data and applies transformations.
-3. Standardizes data by applying default values as well as type casting as applicable.
-4. Calculates row hash using sha256 on all relevant CDC fields.
-5. Performs deduplication based on the row hash of the fully transformed data. This step primarily detects dupes in consecutive rows.
+1. Uses execution_date from Airflow to determine the processing window.
+2. Fetches data till the previous day from the Staging layer, one key hash per day (latest).
+3. Calculates `contract_create_ts` and `contract_delete_ts` from staging data.
+4. Joins the calculated timestamps with the incremental data and applies transformations.
+5. Calculates a row hash using SHA256 on all relevant fields.
+6. Performs deduplication based on the row hash of the fully transformed data. This step primarily detects duplicates in consecutive rows.
 
 Usage:
 ------
@@ -33,88 +34,127 @@ Compile:
 Run:
     dbt run --models int_transform_contract_code --full-refresh
     dbt run --models int_transform_contract_code
+    dbt run --models int_transform_contract_code --vars '{"execution_date": "2024-10-01T00:00:00+00:00"}'
 */
 
+-- Set the execution date (use Airflow value, fallback to dbt default if absent)
+{% set execution_date = var('execution_date', dbt_airflow_macros.ts()) %}
+
 with
-    derived_data as (
+    source_data as (
         select
             contract_code_hash
+            , ledger_key_hash
+            , last_modified_ledger
+            , ledger_entry_change
+            , ledger_sequence
+            , deleted
+            , closed_at
+            , n_instructions
+            , n_functions
+            , n_globals
+            , n_table_entries
+            , n_types
+            , n_data_segments
+            , n_elem_segments
+            , n_imports
+            , n_exports
+            , n_data_segment_bytes
+            , batch_id
+            , batch_run_date
+            , cast('{{ execution_date }}' as timestamp) as airflow_start_ts
+            -- Rank records to identify the latest entry
+            , row_number() over (
+                partition by ledger_key_hash, cast(closed_at as date)
+                order by closed_at desc, ledger_sequence desc
+            ) as row_num
+        from {{ ref('stg_contract_code') }}
+        where
+            -- Process only completed days up to execution_date
+            date(closed_at) < date('{{ execution_date }}')
+    )
+
+    , filtered_data as (
+        select *
+        from source_data
+        where row_num = 1  -- Only consider the latest record
+    )
+
+    , derived_data as (
+        select
+            ledger_key_hash
             , min(case when ledger_entry_change = 0 then closed_at end) as contract_create_ts  -- Calculate contract created timestamp
             -- Calculate contract deletion timestamp
             , max(case when ledger_entry_change = 2 and deleted = true then closed_at end) as contract_delete_ts
-        from {{ ref('stg_contract_code') }}
-        group by contract_code_hash
-    )
-
-    , source_data as (
-        select
-            int.contract_code_hash
-            , int.ledger_key_hash
-            , int.n_instructions
-            , int.n_functions
-            , int.n_globals
-            , int.n_table_entries
-            , int.n_types
-            , int.n_data_segments
-            , int.n_elem_segments
-            , int.n_imports
-            , int.n_exports
-            , int.n_data_segment_bytes
-            , derived.contract_create_ts
-            , derived.contract_delete_ts
-            , int.closed_at
-            , int.ledger_sequence
-            , int.airflow_start_ts
-            , int.batch_id
-            , int.batch_run_date
-        from {{ ref('int_incr_contract_code') }} as int
-        left join derived_data as derived on int.contract_code_hash = derived.contract_code_hash
-    )
-
-    -- Calculate a hash for each row using sha256
-    , hashed_data as (
-        select
-            *
-            , sha256(concat(
-                coalesce(ledger_key_hash, '')
-                , coalesce(cast(n_instructions as string), '')
-                , coalesce(cast(n_functions as string), '')
-                , coalesce(cast(n_globals as string), '')
-                , coalesce(cast(n_table_entries as string), '')
-                , coalesce(cast(n_types as string), '')
-                , coalesce(cast(n_data_segments as string), '')
-                , coalesce(cast(n_elem_segments as string), '')
-                , coalesce(cast(n_imports as string), '')
-                , coalesce(cast(n_exports as string), '')
-                , coalesce(cast(n_data_segment_bytes as string), '')
-                , coalesce(cast(contract_create_ts as string), '')
-                , coalesce(cast(contract_delete_ts as string), '')
-            )) as row_hash
         from source_data
+        group by ledger_key_hash
     )
 
-    -- Identify changes between consecutive records and pick the first record if duplicates
+    , transformed_data as (
+        select
+            f.ledger_key_hash
+            , f.contract_code_hash
+            , f.last_modified_ledger
+            , f.ledger_entry_change
+            , f.ledger_sequence
+            , f.deleted
+            , f.closed_at
+            , f.n_instructions
+            , f.n_functions
+            , f.n_globals
+            , f.n_table_entries
+            , f.n_types
+            , f.n_data_segments
+            , f.n_elem_segments
+            , f.n_imports
+            , f.n_exports
+            , f.n_data_segment_bytes
+            , d.contract_create_ts
+            , d.contract_delete_ts
+            , f.airflow_start_ts
+            , f.batch_id
+            , f.batch_run_date
+            , sha256(concat(
+                coalesce(f.ledger_key_hash, '')
+                , coalesce(cast(f.n_instructions as string), '')
+                , coalesce(cast(f.n_functions as string), '')
+                , coalesce(cast(f.n_globals as string), '')
+                , coalesce(cast(f.n_table_entries as string), '')
+                , coalesce(cast(f.n_types as string), '')
+                , coalesce(cast(f.n_data_segments as string), '')
+                , coalesce(cast(f.n_elem_segments as string), '')
+                , coalesce(cast(f.n_imports as string), '')
+                , coalesce(cast(f.n_exports as string), '')
+                , coalesce(cast(f.n_data_segment_bytes as string), '')
+                , coalesce(cast(d.contract_create_ts as string), '')
+                , coalesce(cast(d.contract_delete_ts as string), '')
+            )) as row_hash
+        from filtered_data as f
+        left join derived_data as d on f.ledger_key_hash = d.ledger_key_hash
+    )
+
     , dedup_data as (
         select
             *
-            -- Get the hash of the previous record for each contract_code_hash
-            , lag(row_hash) over (partition by contract_code_hash order by closed_at, ledger_sequence) as prev_row_hash
+            , lag(row_hash) over (partition by ledger_key_hash order by closed_at, ledger_sequence) as prev_row_hash
             , case
-                when row_hash = lag(row_hash) over (partition by contract_code_hash order by closed_at, ledger_sequence)
+                when row_hash = lag(row_hash) over (partition by ledger_key_hash order by closed_at, ledger_sequence)
                     then 0  -- No change
                 else 1  -- Change
             end as is_change  -- Flag records that are different from their previous record
-        from hashed_data
+        from transformed_data
     )
 
--- Keep records that are different from their previous record (is_change = 1)
--- OR the first record for each ledger_key_hash (prev_row_hash is null)
+-- Select deduplicated records
 select
-    contract_code_hash
-    , ledger_key_hash
+    ledger_key_hash
+    , contract_code_hash
+    , last_modified_ledger
+    , ledger_entry_change
     , ledger_sequence
     , contract_create_ts
     , contract_delete_ts
+    , deleted
     , closed_at
     , n_instructions
     , n_functions
--- models/intermediate/int_transform_ttl.sql
+++ models/intermediate/int_transform_ttl.sql
@@ -17,17 +17,19 @@ Model: int_transform_ttl
 
 Description:
 ------------
-This intermediate model applies transformations to ttl data, calculates new fields,
+This intermediate model combines incremental loading and transformations for ledger TTL data, calculates new fields,
 and performs deduplication based on the transformed data.
 
 Load Type: Truncate and Reload
 
 Features:
 ---------
-1. Calculates ttl_create_ts and ttl_delete_ts from staging data.
-2. Joins the calculated timestamps with the incremental data and applies transformations.
-3. Calculates row hash using sha256 on all relevant fields.
-4. Performs deduplication based on the row hash of the fully transformed data. This step primarily detects dupes in consecutive rows.
+1. Uses execution_date from Airflow to determine the processing window.
+2. Fetches data till the previous day from the Staging layer, one key hash per day (latest).
+3. Calculates `ttl_create_ts` and `ttl_delete_ts` from staging data.
+4. Joins the calculated timestamps with the incremental data and applies transformations.
+5. Calculates a row hash using SHA256 on all relevant fields.
+6. Performs deduplication based on the row hash of the fully transformed data. This step primarily detects duplicates in consecutive rows.
 
 Usage:
 ------
@@ -37,18 +39,56 @@ Compile:
 Run:
     dbt run --models int_transform_ttl --full-refresh
     dbt run --models int_transform_ttl
+    dbt run --models int_transform_ttl --vars '{"execution_date": "2024-10-25T00:00:00+00:00"}'
 */
 
+-- Set the execution date (use Airflow value, fallback to dbt default if absent)
+{% set execution_date = var('execution_date', dbt_airflow_macros.ts()) %}
+
 with
-    derived_data as (
+    -- Extract data from the staging table and rank records based on key_hash and closed_at
+    source_data as (
         select
             key_hash
-            , min(case when ledger_entry_change = 0 then closed_at end) as ttl_create_ts  -- Calculate contract created timestamp
-            , max(case when ledger_entry_change = 2 and deleted = true then closed_at end) as ttl_delete_ts  -- Calculate contract deletion timestamp
+            , live_until_ledger_seq
+            , last_modified_ledger
+            , ledger_entry_change
+            , ledger_sequence
+            , deleted
+            , closed_at
+            , batch_insert_ts
+            , batch_id
+            , batch_run_date
+            , cast('{{ execution_date }}' as timestamp) as airflow_start_ts
+            -- Rank records to identify the latest entry
+            , row_number() over (
+                partition by key_hash, cast(closed_at as date)
+                order by closed_at desc, ledger_sequence desc
+            ) as row_num
         from {{ ref('stg_ttl') }}
+        where
+            -- Process only completed days up to execution_date
+            date(closed_at) < date('{{ execution_date }}')
+    )
+
+    -- Filter to retain only the latest record for each key_hash
+    , filtered_data as (
+        select *
+        from source_data
+        where row_num = 1  -- Only consider the latest record
+    )
+
+    -- Calculate TTL create and delete timestamps
+    , derived_data as (
+        select
+            key_hash
+            , min(case when ledger_entry_change = 0 then closed_at end) as ttl_create_ts
+            , max(case when ledger_entry_change = 2 and deleted = true then closed_at end) as ttl_delete_ts
+        from source_data
         group by key_hash
     )
 
+    -- Prepare transformed data with additional fields
     , transformed_data as (
         select
             ttl.key_hash
@@ -60,39 +100,29 @@ with
             , ttl.batch_id
             , ttl.batch_run_date
             , ttl.airflow_start_ts
-        from {{ ref('int_incr_ttl') }} as ttl
-        left join derived_data as derived on ttl.key_hash = derived.key_hash
-    )
-
-    -- Calculate a hash for each row using sha256
-    , hashed_data as (
-        select
-            *
             , sha256(concat(
-                coalesce(cast(live_until_ledger_seq as string), '')
-                , coalesce(cast(ttl_create_ts as string), '')
-                , coalesce(cast(ttl_delete_ts as string), '')
+                coalesce(cast(ttl.live_until_ledger_seq as string), '')
+                , coalesce(cast(derived.ttl_create_ts as string), '')
+                , coalesce(cast(derived.ttl_delete_ts as string), '')
             )) as row_hash
-        from transformed_data
+        from filtered_data as ttl
+        left join derived_data as derived on ttl.key_hash = derived.key_hash
     )
 
     -- Identify changes between consecutive records and pick the first record if duplicates
     , dedup_data as (
         select
             *
-            -- Get the hash of the previous record for each key_hash
             , lag(row_hash) over (partition by key_hash order by closed_at, ledger_sequence) as prev_row_hash
             , case
                 when row_hash = lag(row_hash) over (partition by key_hash order by closed_at, ledger_sequence)
                     then 0  -- No change
                 else 1  -- Change
             end as is_change  -- Flag records that are different from their previous record
-        from hashed_data
+        from transformed_data
     )
 
 -- Select deduplicated records
--- Keep records that are different from their previous record (is_change = 1)
--- OR the first record for each key_hash (prev_row_hash is null)
 select
     key_hash
     , live_until_ledger_seq
