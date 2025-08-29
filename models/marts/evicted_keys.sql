6a2821a4f829de748079d1e9b88df6fcd87a3ee1
Amisha Singla <amishas157@gmail.com>
2025-08-29 16:44:59 -0500

    Add new model for evicted keys

---
+++ models/marts/evicted_keys.sql
@@ -0,0 +1,42 @@
+{% set meta_config = {
+    "materialized": "incremental",
+    "unique_key": 'ledger_key_hash || "-" || cast(closed_at as string) || "-" || is_evicted',
+    "incremental_strategy": "append",
+    "tags": ["evicted_keys"]
+} %}
+
+{{ config(
+    meta=meta_config,
+    **meta_config,
+    )
+}}
+
+with evicted as (
+    select
+        kh as ledger_key_hash
+        , shl.closed_at
+        , 'yes' as is_evicted
+    from {{ ref('stg_history_ledgers') }} shl
+    cross join unnest(shl.evicted_ledger_keys_hash) as kh
+    {% if is_incremental() %}
+      where true
+        and date(closed_at) >= date('{{ dbt_airflow_macros.ts(timezone=none) }}')
+
+    {% endif %}
+),
+
+restored as (
+    select
+        ledger_key_hash as key_hash,
+        closed_at,
+        'no' as is_evicted
+    from {{ ref('stg_restored_key') }}
+    {% if is_incremental() %}
+      where true
+        and date(closed_at) >= date('{{ dbt_airflow_macros.ts(timezone=none) }}')
+    {% endif %}
+)
+
+select * from evicted
+union all
+select * from restored
