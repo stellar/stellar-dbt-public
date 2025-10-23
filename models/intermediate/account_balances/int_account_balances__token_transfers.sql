c5aff302418deb33f0681474d57f7332ca6b7c57
Simon Chow <simon.chow@stellar.org>
2025-10-23 12:14:27 -0400

    update token transfer balance logic

--- models/intermediate/account_balances/int_account_balances__token_transfers.sql
+++ models/intermediate/account_balances/int_account_balances__token_transfers.sql
@@ -17,27 +17,100 @@
 }}
 
 -- This table computes the daily balance for a given account_id
+{% if not is_incremental() %}
+-- Logic for full refresh
+    with
+        account_date_ranges as (
+            select
+                account_id
+                , contract_id
+                , min(day) as start_day
+                , max(day) as last_change_day
+            from {{ ref('int_account_balances__token_transfers_value_movement') }}
+            where
+                true
+                and day <= date('{{ dbt_airflow_macros.ts(timezone=none) }}')
+            group by 1, 2
+        )
+
+        , date_spine as (
+            select
+                adr.account_id
+                , adr.contract_id
+                , day
+            from account_date_ranges as adr
+            , unnest(generate_date_array(adr.start_day, date('{{ dbt_airflow_macros.ts(timezone=none) }}'))) as day
+        )
+
+        , cumulative as (
+            select
+                day
+                , account_id
+                , contract_id
+                , sum(balance) over (
+                    partition by account_id, contract_id
+                    order by day
+                    rows between unbounded preceding and current row
+                ) as balance_on_day
+            from {{ ref('int_account_balances__token_transfers_value_movement') }}
+            where
+                true
+                and day <= date('{{ dbt_airflow_macros.ts(timezone=none) }}')
+        )
+
+        , daily_balances as (
+            select
+                ds.day
+                , ds.account_id
+                , ds.contract_id
+                , last_value(c.balance_on_change_day ignore nulls) over (
+                    partition by ds.account_id, ds.contract_id
+                    order by ds.day
+                    rows between unbounded preceding and current row
+                ) as balance
+            from date_spine as ds
+            left join cumulative as c
+                on ds.account_id = c.account_id
+                and ds.contract_id = c.contract_id
+                and ds.day = c.day
+        )
+
+{% else %}
+-- Logic for incremental runs
 with
-    dates as (
-        {% if not is_incremental() %}
-            select dates as day
-            from unnest(generate_date_array('2015-09-30', date('{{ dbt_airflow_macros.ts(timezone=none) }}'))) as dates
-        {% else %}
-            select date('{{ dbt_airflow_macros.ts(timezone=none) }}') as day
-        {% endif %}
+    value_movements as (
+        select
+            day
+            , account_id
+            , contract_id
+            , balance
+        from {{ ref('int_account_balances__token_transfers_value_movement') }}
+        where true
+            and day = date('{{ dbt_airflow_macros.ts(timezone=none) }}')
+    )
+
+    , previous_balances as (
+        select
+            day
+            , account_id
+            , contract_id
+            , balance
+        from {{ this }}
+        where true
+            and day = date_sub(date('{{ dbt_airflow_macros.ts(timezone=none) }}'), interval 1 day)
     )
 
     , daily_balances as (
         select
-            d.day
-            , ttvm.account_id
-            , ttvm.contract_id
-            , sum(ttvm.balance) as balance
-        from dates as d
-        left join {{ ref('int_account_balances__token_transfers_value_movement') }} as ttvm
-            on d.day >= ttvm.day
-        group by 1, 2, 3
-    )
+            date('{{ dbt_airflow_macros.ts(timezone=none) }}') as day
+            , pb.account_id
+            , pb.contract_id
+            , pb.balance + coalesce(vm.balance, 0) as balance
+        from previous_balances pb
+        left join value_movements vm
+            on vm.account_id = pb.account_id
+            and vm.contract_id = pb.contract_id
+{% endif %}
 
 select
     db.day
@@ -50,3 +123,4 @@ select
 from daily_balances as db
 left join {{ ref('int_assets') }} as a
     on db.contract_id = a.contract_id
+order by 1, 2
