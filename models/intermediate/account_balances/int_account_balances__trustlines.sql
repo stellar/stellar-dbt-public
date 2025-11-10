cb05d02ddb58f6177ec37495540b1ecab0b381ee
Simon Chow <simon.chow@stellar.org>
2025-11-10 11:53:29 -0500

    fix snapshot window

--- models/intermediate/account_balances/int_account_balances__trustlines.sql
+++ models/intermediate/account_balances/int_account_balances__trustlines.sql
@@ -34,7 +34,7 @@ with
         where
             tl.liquidity_pool_id = ''
             and tl.deleted is false
-            and tl.valid_from <= timestamp((select max(day) from dt))
+            and tl.valid_from < timestamp(date_add((select max(day) from dt), interval 1 day))
             and (tl.valid_to is null or tl.valid_to >= timestamp((select min(day) from dt)))
     )
 
@@ -50,7 +50,7 @@ with
         from {{ ref('accounts_snapshot') }} as acc
         where
             acc.deleted is false
-            and acc.valid_from <= timestamp((select max(day) from dt))
+            and acc.valid_from <= timestamp(date_add((select max(day) from dt), interval 1 day))
             and (acc.valid_to is null or acc.valid_to >= timestamp((select min(day) from dt)))
     )
 
