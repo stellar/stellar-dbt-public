05c454d8fc7c7cae4f535dc2915c871975dd0cb6
Trevor Thayer <trevor.thayer32@gmail.com>
2026-03-09 15:07:35 -0700

    updated fee stat tags for airflow

--- models/marts/daily_fee_stats_agg.sql
+++ models/marts/daily_fee_stats_agg.sql
@@ -1,7 +1,7 @@
 {% set meta_config = {
     "materialized": "incremental",
     "unique_key": ["day_agg"],
-    "tags": ["fee_stats"],
+    "tags": ["daily_fee_stats"],
     "cluster_by": ["day_agg"]
 } %}
 
