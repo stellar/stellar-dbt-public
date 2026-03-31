18215e5e2b09e173b74a3927eb57fbda71928884
Amisha Singla <amishas157@gmail.com>
2026-03-31 16:40:07 -0500

    Add custom recency test with optional timestamp_val parameter
    Replace dbt_utils.recency with a custom recency test that accepts an
    optional timestamp_val to set the threshold relative to a specific
    timestamp instead of current_timestamp(). All 51 model recency tests
    now use this custom test with timestamp_val set to batch_end_date.

---
+++ tests/generic/recency.sql
@@ -0,0 +1,46 @@
+{% test recency(model, field, datepart, interval, ignore_time_component=False, group_by_columns = [], timestamp_val=none) %}
+
+-- ported from https://github.com/dbt-labs/dbt-utils/blob/main/macros/generic_tests/recency.sql
+
+{{ config(severity = 'error') }}
+
+{% if timestamp_val %}
+    {% set threshold = 'cast(' ~ dbt.dateadd(datepart, interval * -1, "cast('" ~ timestamp_val ~ "' as timestamp)") ~ ' as ' ~ ('date' if ignore_time_component else dbt.type_timestamp()) ~ ')' %}
+{% else %}
+    {% set threshold = 'cast(' ~ dbt.dateadd(datepart, interval * -1, dbt.current_timestamp()) ~ ' as ' ~ ('date' if ignore_time_component else dbt.type_timestamp()) ~ ')' %}
+{% endif %}
+
+{% if group_by_columns|length() > 0 %}
+  {% set select_gb_cols = group_by_columns|join(' ,') + ', ' %}
+  {% set groupby_gb_cols = 'group by ' + group_by_columns|join(',') %}
+{% endif %}
+
+
+with recency as (
+
+    select 
+
+      {{ select_gb_cols }}
+      {% if ignore_time_component %}
+        cast(max({{ field }}) as date) as most_recent
+      {%- else %}
+        max({{ field }}) as most_recent
+      {%- endif %}
+
+    from {{ model }}
+
+    {{ groupby_gb_cols }}
+
+)
+
+select
+
+    {{ select_gb_cols }}
+    most_recent,
+    {{ threshold }} as threshold
+
+from recency
+where most_recent < {{ threshold }}
+   or most_recent is null
+
+{% endtest %}
